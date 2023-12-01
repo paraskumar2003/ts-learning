5f0ea102793bce7d8ae1f1dab860412a8dcee59e
Paras Kumar <paras.kumar@almond.solutions>
2023-12-01 18:38:00 +0530

    first commit

--- simple-ts/build/index.js
+++ simple-ts/build/index.js
@@ -2,6 +2,33 @@
 function multiply(a, b) {
     return a * b;
 }
+let n = null;
+let u = undefined;
+const number = 10;
+const string = "Paras";
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+const c = { someProp: 1 };
+const person = { name: "Paras", age: 19, isMale: true, isActive: true, profile: { name: "Paras", age: 20, isMale: true } };
+console.log(c);
+console.log(Color.Red);
+console.log(Color[0]);
+const array = ["paras", "aditya", "roop", "prince"];
+const record = {
+    name: "Paras"
+};
+console.log(record);
+console.log(array);
+let someNumber = 9;
+function capitalize(str) {
+    return str[0].toUpperCase() + str.substring(1);
+}
+console.log(capitalize("hello"));
+console.log(capitalize("hi"));
 const a = 2;
 const b = 5;
 console.log(`${a} * ${b} = ${multiply(a, b)}`);
--- simple-ts/src/index.ts
+++ simple-ts/src/index.ts
@@ -2,6 +2,59 @@ function multiply(a: number, b: number): number {
     return a * b;
 }
 
+let n: null = null;
+let u: undefined = undefined;
+
+const number: number = 10;
+const string: string = "Paras";
+enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+interface A {
+    someProp: number;
+}
+
+interface Profile {
+    name: string;
+    age: number;
+    isMale: boolean;
+}
+
+const c: A = { someProp: 1 };
+
+const person: Record<string, string | number | boolean | Profile> = { name: "Paras", age: 19, isMale: true, isActive: true, profile: { name: "Paras", age: 20, isMale: true } };
+
+console.log(c);
+
+console.log(Color.Red);
+console.log(Color[0]);
+
+const array: Array<string | null> = ["paras", "aditya", "roop", "prince"];
+
+const record: Object = {
+    name: "Paras"
+};
+
+console.log(record);
+
+console.log(array);
+
+
+
+
+let someNumber: number = 9;
+
+function capitalize(str: string): string {
+    return str[0].toUpperCase() + str.substring(1);
+}
+
+console.log(capitalize("hello"));
+console.log(capitalize("hi"));
+
+
 const a = 2;
 const b = 5;
 
